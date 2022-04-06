3204e55e609ab7924d44098590eba369945a8511
Ilya Luzyanin <ilya@adnymics.com>
2022-04-06 13:03:33 +0200

    Create HMR simple issue repro

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- If you change the text below and save, the page will be updated automatically -->
+    Counter value from template: 1 <br />
+    Counter value from script: {{ counter }}
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+// If you change the counter value and save, the page is not updated
+// You need to refresh to see the new value
+const counter = 1;
+</script>
